f8a729336fb48f457644544c642cbc93d137b15e
Kh_ra <khora.seule@protonmail.com>
2022-12-02 08:26:00 -0600

    Day 2 Part 2 Completion

--- Day2/day2.py
+++ Day2/day2.py
@@ -5,19 +5,25 @@ def main( path ):
     transSelf = dict( zip( [ "X", "Y", "Z" ], range( 3 ) ) )
 
     guide = [ line.split() for line in open( path )  ]
-    scores = []
+    naiveScores = []
+    actualScores = []
 
     for round in guide:
 
         opp, self = transOpp[ round[ 0 ] ], transSelf[ round[ 1 ] ]
 
-        score = ( 1 + self ) + 3 * range(3)[ ( self - opp + 1 ) % 3 ]
+        naiveScore = ( 1 + self ) + 3 * range(3)[ ( self - opp + 1 ) % 3 ]
+        actualScore = ( 1 + range(3)[ ( self + opp - 1 ) % 3 ] ) + 3 * self
         
-        scores = scores + [ score ]
-    
-    return sum( scores )
+        naiveScores = naiveScores + [ naiveScore ]
+        actualScores = actualScores + [ actualScore ]
+
+    part1 = sum( naiveScores )
+    part2 = sum( actualScores )
+
+    return ( part1, part2 )
 
 
-assert ( main( "./Day2/test.txt" ) == 15 )
+assert ( main( "./Day2/test.txt" ) == ( 15, 12 ) )
 
 print( main( "./Day2/input.txt" ) )
