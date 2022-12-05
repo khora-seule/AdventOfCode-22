71fca73d012cd0ae0b363db429290d4996a1509d
Kh_ra <khora.seule@protonmail.com>
2022-12-05 08:25:13 -0600

    Day 4 Catchup Completion

---
+++ Day4/day4.py
@@ -0,0 +1,28 @@
+
+def main( path ):
+
+    pairAssignments = [ [ [ int( assignment ) for assignment in assignments.strip().split( "-" ) ] for assignments in line.split( "," ) ] for line in open( path ) ]
+
+    totalOverlaps = 0
+    overlaps = 0
+
+    for pair in pairAssignments:
+        
+        firstPair = set( range( pair[ 0 ][ 0 ], pair[ 0 ][ 1 ] + 1 ) )
+        secondPair = set( range( pair[ 1 ][ 0 ], pair[ 1 ][ 1 ] + 1) )
+        overlap = firstPair & secondPair
+
+        if( overlap in [ firstPair, secondPair ] ):
+            totalOverlaps += 1
+
+        if( overlap != set() ):
+            overlaps += 1
+
+
+
+    return ( totalOverlaps, overlaps )
+
+
+assert ( main( "./Day4/test.txt" ) == ( 2, 4 ) )
+
+print( main( "./Day4/input.txt" ) )
