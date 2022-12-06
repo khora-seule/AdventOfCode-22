a12b14d6c96bd7f518acc54620982ee27c02a79a
Kh_ra <khora.seule@protonmail.com>
2022-12-06 11:59:01 -0600

    Day 6 Completion

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,9 +1,30 @@
 def main( path ):
 
-    return
+    answerPt1 = 0
+    answerPt2 = 0
+
+    with open( path ) as file:
+
+        signal = file.read().rstrip()
+
+        index = 0
+
+        while( answerPt1 == 0 ):
+            if( len( set( signal[ index : index + 4 : 1 ] ) ) == 4 ):
+                answerPt1 = index + 4
+            index += 1
+
+        index = 0
+
+        while( answerPt2 == 0 ):
+            if( len( set( signal[ index : index + 14 : 1 ] ) ) == 14 ):
+                answerPt2 = index + 14
+            index += 1
+
+    return ( answerPt1, answerPt2 )
 
 
-testAnswers = [ 7, 5, 6, 10, 11 ]
+testAnswers = [ ( 7, 19 ), ( 5, 23 ), ( 6, 23 ), ( 10, 29 ), ( 11, 26 ) ]
 
 for i in range( len( testAnswers ) ):
     assert ( main( "./Day6/test" + str( i ) + ".txt" ) == testAnswers[ i ] )
